f69be24c21677c1e1e959977ac81bed94330ab14
Walter Gandarella <walter.wgbn@gmail.com>
2017-03-28 21:54:47 -0300

    checkin-manual

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,8 @@
 import {Component} from '@angular/core';
-import {AngularFire, FirebaseObjectObservable} from 'angularfire2';
+import {AngularFire, FirebaseObjectObservable, FirebaseListObservable} from 'angularfire2';
 
 import {BarcodeScanner} from "@ionic-native/barcode-scanner";
+import {AlertController, LoadingController} from "ionic-angular";
 
 @Component({
     selector: 'page-home',
@@ -9,11 +10,37 @@ import {BarcodeScanner} from "@ionic-native/barcode-scanner";
 })
 export class HomePage {
 
-    msg: string = '';
+    msg: string = ' ';
+    inscritos: Array<any>;
+    busca: string = '';
+    private inscritosFull: Array<any>;
     private check: FirebaseObjectObservable<any>;
     private checkObj: any;
 
-    constructor(private fire: AngularFire, private barcodeScanner: BarcodeScanner) { }
+    constructor(
+        private fire: AngularFire,
+        private barcodeScanner: BarcodeScanner,
+        private alertCtrl: AlertController,
+        private loadingCtrl: LoadingController) {
+        this.loadInscritos();
+    }
+
+    private loadInscritos(): void {
+        let loader = this.loadingCtrl.create({
+            content: "Carregando...",
+            //duration: 3000
+        });
+        loader.present();
+
+        this.fire.database.list('/inscricoes').subscribe(
+            res => {
+                loader.dismissAll();
+                this.inscritos = res;
+                this.inscritosFull = res;
+                this.filtrar();
+            }
+        );
+    }
 
     fazerCheckIn(): void {
         this.barcodeScanner.scan().then((barcodeData) => {
@@ -31,14 +58,80 @@ export class HomePage {
         });
     }
 
+    private alert(titulo, msg): void {
+        let alert = this.alertCtrl.create({
+            title: titulo,
+            subTitle: msg,
+            buttons: ['OK']
+        });
+        alert.present();
+    }
+
     private updateCheck(): void {
         if (!this.checkObj.checkin) {
             this.msg = 'registrano...';
             this.check.update({checkin: new Date().getTime()})
                 .catch(err => console.error(err));
         } else {
-            setTimeout( () => this.msg = this.checkObj.name + ' - check-in OK!', 150);
-            setTimeout( () => this.msg = '', 1500);
+            this.alert('Check-in', this.checkObj.name + ' - check-in OK!');
         }
+    }
+
+    checkIn(insc: any): void {
+        let confirm = this.alertCtrl.create({
+            title: 'Confirmar '+(insc.checkin ? 'ausência':'presença')+'?',
+            message: 'Confirmar '+(insc.checkin ? 'ausência':'presença')+' de '+insc.name+'?',
+            buttons: [
+                {
+                    text: 'cancelar',
+                    handler: () => {
+                        console.log('Disagree clicked');
+                    }
+                },
+                {
+                    text: 'confirmar',
+                    handler: () => {
+                        this.fire.database.list('/inscricoes').update(insc.$key, {checkin: insc.checkin ? null : new Date().getTime()}).then(
+                            () => this.loadInscritos()
+                        ).catch( err => console.error(err));
+                        /*if (!insc.checkin)
+                            this.fire.database.list('/inscricoes').update(insc.$key, {checkin: new Date().getTime()}).then(
+                                () => this.loadInscritos()
+                            ).catch( err => console.error(err));
+                        else
+                            this.alert('Chack-in', insc.name.split(' ')+ ' já fez check-in');*/
+                    }
+                }
+            ]
+        });
+        confirm.present();
+    }
+
+    delete(insc: any): void {
+        let confirm = this.alertCtrl.create({
+            title: 'Excluir inscrição?',
+            message: 'Tem certeza que quer excluir a inscrição de '+insc.name+'?',
+            buttons: [
+                {
+                    text: 'cancelar',
+                    handler: () => {
+                        console.log('Disagree clicked');
+                    }
+                },
+                {
+                    text: 'excluir',
+                    handler: () => {
+                        this.fire.database.list('/inscricoes').remove(insc).then(
+                            () => this.loadInscritos()
+                        ).catch( err => console.error(err));
+                    }
+                }
+            ]
+        });
+        confirm.present();
+    }
+
+    filtrar(): void {
+        this.inscritos = this.inscritosFull.filter( i => i.name.toLowerCase().indexOf(this.busca.toLowerCase()) > -1);
     }
 }
